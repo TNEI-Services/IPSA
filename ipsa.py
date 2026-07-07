6313208557adf7a65b147832a107057d84b0dd39
Mengya Li <156085981+mengya-li-tnei@users.noreply.github.com>
2026-07-07 11:53:29 +0100

    Correct the description of GetLFSummaryResults
    https://github.com/TNEI-Services/IPSA-Project/issues/1090
    related PyIPSA documentation update

--- ipsa.py
+++ ipsa.py
@@ -19726,9 +19726,19 @@ class IscNetwork:
         """
         pass
 
-    def GetLFSummaryResults(self) -> None:
+    def GetLFSummaryResults(self) -> bool:
         """
-        Call this function to obtain the load flow summary results.
+        Call this function to prepare the summary of load flow results. If a load flow has been since the last call, this must be called again to update the summary to the most recent results. The summary results contain:
+
+            - The highest and lowest busbar voltage, in PU
+            - Total active and reactive power of all generators (excluding those on slack busbar)
+            - Total active and reactive power of all loads
+            - Total active and reactive power of all induction machines
+            - Total active and reactive power of all universal machines
+            - Total active and reactive power on all slack busbars
+        
+        :return: True if load flow results exist, False if no load flow results are available.
+        :rtype: bool
         """
         pass
 
